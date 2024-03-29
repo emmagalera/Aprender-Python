e8b3b58c4fe33ab92365a84f73550d6b6020e223
emmagalera <56721602+emmagalera@users.noreply.github.com>
2024-03-29 17:37:53 +0100

    Update listas.py

--- listas.py
+++ listas.py
@@ -152,7 +152,7 @@ print(f"Hay {contador} elementos en la lista \n")
 
 #Bucle anidados: por cada iteración del bucle externo, el bucle interno se recorrerá completamente
 dias_semana = ["L", "M", "x", "j", "V"] #bucle externo
-tareas_semana = ["Beibol", "Danza", "Natación"] #bucle interno
+tareas_semana = ["Beisbol", "Danza", "Natación"] #bucle interno
 for dia in dias_semana:
     for tarea in tareas_semana:
         print(f"{dia}: {tarea}")
@@ -163,3 +163,81 @@ for dia in dias_semana:
     for tarea in tareas_semana:
         print(tarea)
     print("\n")
+
+#Verificar si un elemento esta en una lista o no
+print("Beisbol" in tareas_semana) #true
+print("Beisbol" not in tareas_semana) #Falsxe
+print("Bailar" not in tareas_semana) #True
+
+#Encontrar el mayor valor
+lista_numeros = [6, 7, 8, 10, 9]
+mayor = lista_numeros[0]
+for i in lista_numeros[1:]: 
+    if i > mayor:
+        mayor = i
+print(f"\nEl elemento con mayor valor es: {mayor}")
+
+#El bucle tambien puede ser:
+"""
+a la varible menor le damos el valor del primer elemento de la lista
+En el bucle for el rango es desde 1 hasta el numero  que supone la longitud de la lista
+Si elemento de la lista de la posicion actual, en este caso lista_numeros[i] donde i 
+es 1 por ejemplo, es menor al valor que tiene la variable actual, entonces
+se actualiza el valor de la variable menor, por elemento de la lista de la posicion actual,
+es decir, lista_numeros[i]
+Nota: empezamos por el 1 para que lista_numeros[i] sea lista_numeros[1] y no lista_numeros[0],
+por el valor de lista_numeros[0] ya esta dado a la variable menor
+"""
+menor = lista_numeros[0]
+for i in range(1, len(lista_numeros)):
+    if lista_numeros[1] < menor:
+        menor = lista_numeros[i]
+print(f"\nEl elemento con valor menor es: {menor}\n")
+
+#Encontrar la posicion de un elemento en una lista
+#lista_numeros = [6, 7, 8, 10, 9]
+encontrar = 9
+encontrado = False
+for posicion in range(len(lista_numeros)): #la longitud de la lista es 5, entonces se cuenta de 0 a 4
+    print(f"Posicion: {posicion} tiene el valor {lista_numeros[posicion]}")
+    if lista_numeros[posicion] == encontrar:
+        encontrado = True
+        break
+if encontrado:
+    print(f"El numero {encontrar} esta en la posicion {posicion}")
+else:
+    print(f"No se ha localizado el numero {encontrar} en la lista {lista_numeros}")
+
+#Compresion de lista
+"""
+Creacion de una lista llamada numeros que contine el numero 1 repetido 3 veces
+"""
+numeros = [1 for i in range(3)]
+print(numeros, "\n")
+#El codigo de arriba es lo mismo que
+"""
+numeros = []
+for i in range(3):
+    numeros.append(1)
+"""
+
+#Aumentar de 2 en 2, empezando a contar desde el 2
+#Codigo comprimido: numeros = [2 + i*2 for i in range (10)]
+print("Aumentar de 2 en 2, empezando a contar desde el 2")
+print("--------------------------------------------------")
+numeros = []
+for i in range(10):
+    numeros.append(2 + i*2)
+    print(f"2 + ({i} * 2 = {i*2}) = {numeros[i]}")
+print(f"\nResultado: {numeros} \n")
+
+#Aumentar de 1 en 1, comenzando desde el numero 2
+#Codigo comprimdo: numeros = [2 + i for i in range(5)]
+print("Aumentar de 1 en 1, comenzando desde el numero 2")
+print("-------------------------------------------------\n")
+numeros = []
+for i in range(5):
+    numeros.append(2 + 1)
+    print(f"2 + {i} =  {2 + i}")
+print(f"\nResultado: {numeros}\n")
+
